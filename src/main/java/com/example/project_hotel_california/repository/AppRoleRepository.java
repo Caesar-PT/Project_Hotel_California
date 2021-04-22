42843ce8e9f6c34ed760eab6feeafe6659273f2e
vantu <nguyenvantu2972002@gmail.com>
2021-04-22 20:07:44 +0700

    tu

---
+++ src/main/java/com/example/project_hotel_california/repository/AppRoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.project_hotel_california.repository;
+
+import com.example.project_hotel_california.model.AppRole;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AppRoleRepository extends CrudRepository<AppRole, Long> {
+}
