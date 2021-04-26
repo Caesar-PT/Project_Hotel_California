1a6b7fd78b8539b9e25aa4839a847c3f5d933da9
Caesar-PT <73921520+Caesar-PT@users.noreply.github.com>
2021-04-26 09:41:04 +0700

    service controller post

---
+++ src/main/java/com/example/project_hotel_california/service/post/PostService.java
@@ -0,0 +1,34 @@
+package com.example.project_hotel_california.service.post;
+
+import com.example.project_hotel_california.model.Post;
+import com.example.project_hotel_california.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService implements IPostService<Post>{
+    @Autowired
+    private PostRepository postRepository;
+
+    @Override
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    @Override
+    public Post findById(Long id) {
+        return postRepository.findById(id).get();
+    }
+
+    @Override
+    public Post save(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Override
+    public void remove(Long id) {
+        postRepository.deleteById(id);
+    }
+}
